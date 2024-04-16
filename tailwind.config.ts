25f30aedfe0a0098bc5e7f132d83304d826e3a4b
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-04-16 21:28:44 +0100

    Update tailwind.config.ts with new font sizes and letter spacing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,19 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontSize: {
+        custom: ["32px", "40px"], // The second value is for line-height
+        custom_1: ["24px", "32px"],
+        custom_2: ["20px", "28px"],
+        custom_3: ["10px", "12px"],
+        custom_4: ["18px", "22px"],
+        custom_5: ["10px", "14px"],
+        custom_6: ["10px", "auto"],
+        custom_7: ["8px", "auto"],
+      },
+      letterSpacing: {
+        custom: "-0.02em",
+      },
     },
   },
   plugins: [],
