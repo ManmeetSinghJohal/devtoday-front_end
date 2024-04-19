c532412671b6b8bd0b310e5955eea59cbb4f4c90
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-04-19 09:51:28 +0100

    Refactor SignUp page UI and add new SVG icons for moon, inbox, sun, and feedback

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -18,9 +18,9 @@ const SignUp = async () => {
   return (
     <div className="mb-24 w-full max-w-[400px] lg:mt-[100px]">
       <div className="flex flex-col items-center">
-        <div className="hidden w-[1440px] border border-red-500 lg:grid lg:grid-cols-2">
-          <div className="border">
-            <div className="mb-[52px] flex">
+        <div className="hidden w-[1440px] lg:grid lg:grid-cols-2">
+          <div className="bg-white-100">
+            <div className="mb-[52px] ml-10 mt-11 flex">
               <Image
                 src="assets/logos/logo.svg"
                 alt="logo"
@@ -36,9 +36,56 @@ const SignUp = async () => {
                 className="hidden dark:block"
               />
             </div>
+            <div className="ml-[148px] mr-[130px] mt-[86px]">
+              <div className="display-1-bold mb-10 text-dark-700 dark:text-white-100">
+                Join our developer community! Sign up now and be part of the
+                conversation.
+              </div>
+              <div className="flex rounded-lg bg-white-200 p-5">
+                <div className="mr-5 flex size-[60px] justify-center rounded-lg bg-misc-100">
+                  <Image
+                    src="assets/icons/briefcase.svg"
+                    width={20}
+                    height={20}
+                    alt="briefcase"
+                  />
+                </div>
+                <div className="paragraph-1-medium w-[322px] pt-2 text-dark-700">
+                  Discover the latest trends, tools, and insights shaping the
+                  developer world.
+                </div>
+              </div>
+              <div className="mt-5 flex rounded-lg bg-white-200 p-5">
+                <div className="mr-5 flex size-[60px] justify-center rounded-lg bg-misc-200">
+                  <Image
+                    src="assets/icons/feedback.svg"
+                    width={20}
+                    height={20}
+                    alt="briefcase"
+                  />
+                </div>
+                <div className="paragraph-1-medium w-[322px] pt-2 text-dark-700">
+                  Forge connections, collaborate on projects, and grow together.
+                </div>
+              </div>
+              <div className="mt-5 flex rounded-lg bg-white-200 p-5">
+                <div className="mr-5 flex size-[60px] justify-center rounded-lg bg-misc-300">
+                  <Image
+                    src="assets/icons/inbox.svg"
+                    width={20}
+                    height={20}
+                    alt="briefcase"
+                  />
+                </div>
+                <div className="paragraph-1-medium w-[322px] pt-2 text-dark-700">
+                  Elevate your coding with exclusive content for professional
+                  growth
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="flex justify-center border border-green-500">
-            <div className="mb-[174px] ml-[130px] mr-[169px] mt-[160px] flex w-full flex-col gap-2 border">
+          <div className="flex justify-center">
+            <div className="mb-[174px] ml-[130px] mr-[169px] mt-[160px] flex w-full flex-col gap-2">
               <CreateUserForm />
               <Link
                 className="paragraph-3-regular mt-3 text-center text-dark-700 dark:text-white-300"
