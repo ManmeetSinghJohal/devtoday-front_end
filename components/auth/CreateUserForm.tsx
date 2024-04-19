5c1ae321dccd1c3c6931302b0aba20ece50e37be
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-04-19 19:19:03 +0100

    Refactor SignUp page UI and add CreateUserForm component for authentication

---
+++ components/auth/CreateUserForm.tsx
@@ -0,0 +1,160 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { useForm } from "react-hook-form";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { TSignUpSchema, signUpSchema } from "@/lib/validations";
+
+const CreateUserForm = () => {
+  const router = useRouter();
+
+  const form = useForm<TSignUpSchema>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  async function onSubmit(values: TSignUpSchema) {
+    console.log("sign-up values", values);
+    try {
+
+      const res = await fetch("http://localhost:3005/auth/register", {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      });
+
+      if (res.ok) {
+        console.log("User created successfully");
+        // router.push("/signin");
+      } else {
+        console.log("Error creating user");
+      }
+    } catch (error) {
+      form.setError("root", {
+        message: "Something went wrong. Please try again later.",
+      });
+    }
+  }
+
+  return (
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="paragraph-3-medium text-dark-800 dark:text-white-200">
+                  Choose a username
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    type="text"
+                    placeholder="Enter a username"
+                    className="paragraph-3-regular h-11 rounded-lg border-white-border px-3 py-3.5 text-dark-900 dark:border-dark-border dark:bg-dark-800 dark:text-white-100"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="paragraph-3-medium text-dark-800 dark:text-white-200">
+                  Email
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    placeholder="Enter your email address"
+                    className="paragraph-3-regular h-11 rounded-lg border-white-border px-3 py-3.5 text-dark-900 dark:border-dark-border dark:bg-dark-800 dark:text-white-100"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="paragraph-3-medium text-dark-800 dark:text-white-200">
+                  Password
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="Enter your password"
+                    className="paragraph-3-regular h-11 rounded-lg border-white-border px-3 py-3.5 text-dark-900 dark:border-dark-border dark:bg-dark-800 dark:text-white-100"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="confirmPassword"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="paragraph-3-medium text-dark-800 dark:text-white-200">
+                  Confirm Password
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="Confirm your password"
+                    className="paragraph-3-regular h-11 rounded-lg border-white-border px-3 py-3.5 text-dark-900 dark:border-dark-border dark:bg-dark-800 dark:text-white-100"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="py-1">
+            <Button
+              className="paragraph-2-bold h-11 w-full rounded-lg bg-primary1-500 dark:text-white-200"
+              type="submit"
+              disabled={form.formState.isSubmitting}
+            >
+              Next
+            </Button>
+          </div>
+        </form>
+      </Form>
+      {form.formState.errors.root && (
+        <p className="text-red-500">{form.formState.errors.root.message}</p>
+      )}
+    </>
+  );
+};
+
+export default CreateUserForm;
