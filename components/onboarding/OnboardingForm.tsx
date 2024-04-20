6cd8ed761bab1784c3d48bae4cbcd4a7bfd63025
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-04-20 18:04:33 +0100

    Refactor OnboardingForm to use zod instead of zodResolver

--- components/onboarding/OnboardingForm.tsx
+++ components/onboarding/OnboardingForm.tsx
@@ -2,7 +2,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import React, { useState } from "react";
 import { useFieldArray, useForm } from "react-hook-form";
-import { string, z } from "zod";
+import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
 import {
