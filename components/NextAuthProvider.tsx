d1e4c93e1c6019d3a7a1d6e781ebf574d373d94b
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-04-24 17:07:41 +0100

    Refactor NextAuthProvider component to use TypeScript for children prop

--- components/NextAuthProvider.tsx
+++ components/NextAuthProvider.tsx
@@ -2,7 +2,7 @@
 import { SessionProvider } from "next-auth/react";
 import React from "react";
 
-const NextAuthProvider = ({children}) => {
+const NextAuthProvider = ({children}: {children: React.ReactNode}) => {
   return <SessionProvider>{children}</SessionProvider>;
 };
 
