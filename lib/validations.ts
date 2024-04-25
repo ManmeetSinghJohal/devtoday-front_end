44a206e2d0882558ccc4c4f4f3ba8241d7b3a288
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-04-25 15:10:49 +0100

    Update validation rules for signUpSchema in validations.ts

--- lib/validations.ts
+++ lib/validations.ts
@@ -2,10 +2,19 @@ import { z } from "zod";
 
 export const signUpSchema = z
   .object({
-    username: z.string().min(2).max(50),
-    email: z.string().email(),
-    password: z.string().min(8).max(50),
-    confirmPassword: z.string().min(8).max(50),
+    username: z
+      .string()
+      .min(4, { message: "Username must be between 4 and 20 characters" })
+      .max(20),
+    email: z.string({ required_error: "is required" }).email("Must be valid"),
+    password: z
+      .string()
+      .min(8, { message: "Must be at least 8 characters long." })
+      .max(10),
+    confirmPassword: z
+      .string()
+      .min(8, { message: "Must be at least 8 characters long." })
+      .max(10),
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: "Passwords do not match",
