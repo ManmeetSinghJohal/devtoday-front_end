342fb4333c99d0e976e1ef73388ac40804c2bf83
toiaa <victoria@geddes.com.ar>
2024-04-27 11:20:15 -0300

    hover logout and pre-push

--- components/shared/Navbar/Account.tsx
+++ components/shared/Navbar/Account.tsx
@@ -46,15 +46,15 @@ const Account = () => {
             </div>
           </MenubarTrigger>
           <MenubarContent className="paragraph-3-medium mr-5 mt-3.5 flex w-[174px] flex-col gap-3.5 border-white-border p-5 shadow-none dark:border-dark-700 dark:bg-dark-800 md:rounded-t-none md:border-t-0">
-            <MenubarItem className="flex cursor-pointer gap-2.5 px-0">
-              <Link href="/profile">
+            <MenubarItem className="cursor-pointer gap-2.5 px-0">
+              <Link href="/profile" className="flex gap-2.5">
                 <ProfileIcon />
                 Profile
               </Link>
             </MenubarItem>
 
             <MenubarItem
-              className="flex cursor-pointer gap-2.5 px-0 text-primary1-500 hover:text-primary1-500"
+              className="flex cursor-pointer gap-2.5 px-0 text-primary1-500 hover:text-primary1-500  dark:hover:text-primary1-500"
               onClick={() => signOut()}
             >
               <Image
@@ -66,7 +66,7 @@ const Account = () => {
               <p className=" hover:text-primary1-500">Log out</p>
             </MenubarItem>
             <Separator className="p-0 dark:bg-dark-700" />
-            <MenubarItem className="flex w-full justify-between px-0 font-semibold">
+            <MenubarItem className="flex w-full justify-between p-0 font-semibold">
               Interface
               <div className="flex items-center justify-center gap-1.5 rounded-2xl bg-white-200 p-1 dark:bg-transparent">
                 <div
