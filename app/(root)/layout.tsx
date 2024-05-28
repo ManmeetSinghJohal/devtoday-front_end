e33c66fb18e185254ad76d59c5fafeb373da9031
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-05-28 07:23:43 +0100

    modify css padding

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,7 +4,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <section>
       <Header />
-      <div className="size-full bg-white-200 px-10 py-5 dark:bg-dark-900">
+      <div className="size-full bg-white-200 px-[14px] py-10 dark:bg-dark-900 sm:px-10 sm:py-5">
         {children}
       </div>
     </section>
