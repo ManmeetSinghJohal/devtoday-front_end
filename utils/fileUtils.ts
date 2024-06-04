a2bc45b1cdccd493693389be90041704b0c64bd8
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-06-04 21:00:49 +0100

    add utility function that can be used to delete audio and image files from uploadthing

---
+++ utils/fileUtils.ts
@@ -0,0 +1,32 @@
+export const handleFileDelete = (value, setFileIsDeleting, setValue, toast) => {
+  setFileIsDeleting(true);
+  const fileKey = value.substring(value.lastIndexOf("/") + 1);
+
+  fetch("/api/uploadthing/delete", {
+    method: "POST",
+    mode: "cors",
+    headers: { "Content-Type": "applicaton/json" },
+    body: JSON.stringify({ fileKey }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.success) {
+        setValue("");
+        toast({
+          variant: "success",
+          description: "File removed",
+        });
+      } else {
+        throw new Error("Deletion failed");
+      }
+    })
+    .catch(() => {
+      toast({
+        variant: "destructive",
+        description: "Something went wrong",
+      });
+    })
+    .finally(() => {
+      setFileIsDeleting(false);
+    });
+};
