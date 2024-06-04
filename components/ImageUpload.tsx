e29c418e49f22e4fc3ab2ef7c31b70559a05a7b6
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-06-04 12:57:54 +0100

    conditionally render image uploaded and add toast on succesful upload and on error

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -1,35 +1,40 @@
-
 "use client";
 import Image from "next/image";
 
+import { useToast } from "@/components/ui/use-toast";
 import { UploadDropzone } from "@/utils/uploadthing";
 
-const ImageUpload = ({value, setValue}) => {
-
+const ImageUpload = ({ value, setValue }) => {
+  const { toast } = useToast();
 
   return (
-    <div>
-      <UploadDropzone
-        appearance={{
-          container: "border-white-300",
-        }}
-        endpoint="imageUploader"
-        onClientUploadComplete={(res) => {
-          // Do something with the response
-          setValue(res[0].url);
-        }}
-        onUploadError={(error: Error) => {
-          // Do something with the error.
-          alert(`ERROR! ${error.message}`);
-        }}
-      />
-
+    <>
       {value.length ? (
-        <div>
-          <Image src={value} alt="my image" width={500} height={300} />
+        <div className="relative h-[250px] overflow-hidden">
+          <Image src={value} alt="my image" layout="fill" objectFit="cover" />
         </div>
-      ) : null}
-    </div>
+      ) : (
+        <UploadDropzone
+          appearance={{
+            container: "border-white-300",
+          }}
+          endpoint="imageUploader"
+          onClientUploadComplete={(res) => {
+            setValue(res[0].url);
+            toast({
+              variant: "success",
+              description: "🎉 Upload Completed",
+            });
+          }}
+          onUploadError={(error: Error) => {
+            toast({
+              variant: "destructive",
+              description: `ERROR! ${error.message}`,
+            });
+          }}
+        />
+      )}
+    </>
   );
 };
 
