975ce3398b23bdd9d9045ee199079862a45eab59
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-06-04 13:04:35 +0100

    configure middleware for session userId and add routes for imageUploader and audioUploader

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,36 @@
+import { getServerSession } from "next-auth";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+
+import { authOptions } from "@/lib/auth";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+  imageUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+    .middleware(async () => {
+      const session = await getServerSession(authOptions);
+      if (!session) throw new UploadThingError("Unauthorized");
+      return { userId: session?.user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      console.log("Upload complete for userId:", metadata.userId);
+      console.log("file url", file.url);
+      return { uploadedBy: metadata.userId };
+    }),
+  audioUploader: f({
+    audio: { maxFileSize: "256MB", maxFileCount: 1 },
+  })
+    .middleware(async () => {
+      const session = await getServerSession(authOptions);
+      if (!session) throw new UploadThingError("Unauthorized");
+      return { userId: session?.user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      console.log("Upload complete for userId:", metadata.userId);
+      console.log("file url", file.url);
+      return { uploadedBy: metadata.userId };
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
