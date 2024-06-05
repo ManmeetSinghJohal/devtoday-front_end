e4efc91349deed31a3ec2636562fc28a99f52d40
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-06-05 09:50:23 +0100

    update css styles for success

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -33,7 +33,7 @@ const toastVariants = cva(
         default: "border bg-background text-foreground",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
-        success: "text-white border bg-indigo-600",
+        success: "border bg-primary1-500 text-white-100",
       },
     },
     defaultVariants: {
