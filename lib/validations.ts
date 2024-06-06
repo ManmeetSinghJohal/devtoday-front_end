4cd5e0c7ed0e78231dfd64e9ba9f9f47346e0376
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-06-06 14:55:35 +0100

    reorder, update creatType values to uppercase, rename interestTech

--- lib/validations.ts
+++ lib/validations.ts
@@ -28,19 +28,19 @@ export const createPostSchema = z.object({
     .string()
     .min(4, { message: "Title must be between 4 and 20 characters" })
     .max(20),
-  createType: z.enum(["Post", "Meetup", "Podcast"]),
+  createType: z.enum(["STANDARD", "MEETUP", "PODCAST"]),
   group: z.string(),
-  meetupLocation: z.string().optional(),
   coverImage: z.string().optional(),
   audioFile: z.string().optional(),
   audioTitle: z.string().optional(),
+  meetupLocation: z.string().optional(),
   meetupDate: z
     .date({
       required_error: "A date for the meetup is required.",
     })
     .optional(),
   tinyContent: z.string(),
-  interestTech: z
+  interestTechTags: z
     .array(z.string())
     .max(7, { message: "You can only add 7 tags" }),
 });
