667f7ef972d1ef9d1b3df48479ef742fb483612f
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-06-10 14:21:15 +0100

    Component which will allows you to share a post.

---
+++ components/post-details/SharePost.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import React from "react";
+
+const SharePost = () => {
+  return (
+    <div className="hidden w-full justify-center rounded-md bg-white-border py-2.5 lg:flex">
+      <Image
+        src="../assets/icons/share-arrow.svg"
+        height={18}
+        width={18}
+        alt="share"
+      />
+      <div className="ml-[5px]">Share Post</div>
+    </div>
+  );
+};
+
+export default SharePost;
