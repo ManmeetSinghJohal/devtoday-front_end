eea3b2f3e78b89a780e7163f72b37ac1876b9eb3
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-06-11 18:27:46 +0100

    Modify css and Image src

--- components/post-details/PostStats.tsx
+++ components/post-details/PostStats.tsx
@@ -3,14 +3,9 @@ import React from "react";
 
 const PostStats = () => {
   return (
-    <div className="flex w-full flex-col justify-start gap-5 rounded-2xl border bg-white-200 py-5 pl-5 dark:bg-dark-800 lg:w-[210px] lg:shrink-0 ">
+    <div className="flex w-full flex-col justify-start gap-5 rounded-2xl bg-white-200 py-5 pl-5 dark:bg-dark-800 lg:w-[210px] lg:shrink-0 ">
       <div className="flex">
-        <Image
-          src="/assets/icons/like.svg"
-          height={18}
-          width={18}
-          alt="like"
-        />
+        <Image src="/assets/icons/like.svg" height={18} width={18} alt="like" />
         <div className="paragraph-2-medium ml-2.5 text-white-400">
           24,056 Likes
         </div>
@@ -27,12 +22,7 @@ const PostStats = () => {
         </div>
       </div>
       <div className="flex">
-        <Image
-          src="/assets/preview.svg"
-          height={18}
-          width={18}
-          alt="preview"
-        />
+        <Image src="/assets/mdi_eye.svg" height={18} width={18} alt="preview" />
         <div className="paragraph-2-medium ml-2.5 text-white-400">
           51,212 Views
         </div>
