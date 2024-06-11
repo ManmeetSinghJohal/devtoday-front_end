4cb4cc38e098c9193fb7caa67e7a446bd63dc447
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-06-11 18:26:08 +0100

    Mobile navigation component

---
+++ components/shared/NavigationMobile.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { NavIcons } from "@/constants/header";
+
+export const NavigationMobile = () => {
+  const pathname = usePathname();
+
+  return (
+    <div className="mx-auto flex justify-between gap-2.5 bg-white-100 px-5 py-[18px]">
+      {NavIcons.map(({ id, href, icon: Icon, exact }) => {
+        return (
+          <Link
+            href={href && href}
+            className={`${(exact ? href === pathname : pathname.startsWith(href)) ? "bg-primary1-500 text-white-100" : "bg-transparent text-white-400 dark:text-white-300"} flex items-center justify-center rounded-[7px] p-2.5`}
+            key={id}
+          >
+            <Icon id={id} />
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
