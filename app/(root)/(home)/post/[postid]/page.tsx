15d38eb95f28cdfce69fc87304b3bbc1d10c9c6c
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-06-11 18:47:29 +0100

    Edit a post

--- app/(root)/(home)/post/[postid]/page.tsx
+++ app/(root)/(home)/post/[postid]/page.tsx
@@ -1,7 +1,45 @@
+import { getServerSession } from "next-auth";
 import React from "react";
 
-const EditPost = () => {
-  return <div>EditPost</div>;
+import CreatePostForm from "@/components/create/CreatePostForm";
+import { authOptions } from "@/lib/auth";
+
+interface ParamsProps {
+  params: { postid: string };
+}
+
+
+const EditPost = async ({ params }: ParamsProps) => {
+  const session = await getServerSession(authOptions);
+  const userId = session?.user.id;
+  const resGroups = await fetch(
+    `http://localhost:3005/api/user/${userId}/groups`,
+    {
+      method: "GET",
+      mode: "cors",
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+
+    const post = await fetch(
+      `http://localhost:3005/api/post/${params.postid}`,
+      {
+        method: "GET",
+        mode: "cors",
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+    const postData = await post.json();
+
+  const groupNames = await resGroups.json();
+
+  return (
+    <CreatePostForm
+      groupNames={resGroups.ok ? groupNames : null}
+      authorId={userId}
+      postData={postData}
+    />
+  );
 };
 
 export default EditPost;
