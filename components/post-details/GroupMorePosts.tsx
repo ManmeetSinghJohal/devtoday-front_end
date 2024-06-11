1224faaf224916c692e27bc87facf2943c862ab2
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-06-11 18:33:23 +0100

    A component which will display additional posts from the same group as the post being viewed.

--- components/post-details/GroupMorePosts.tsx
+++ components/post-details/GroupMorePosts.tsx
@@ -1,7 +1,73 @@
+import Image from "next/image";
 import React from "react";
 
 const GroupMorePosts = () => {
-  return <div>GroupMorePosts</div>;
+  return (
+    <div className="w-full space-y-5 rounded-2xl bg-white-200 p-5">
+      <div className="flex gap-[3px]">
+        <div className="paragraph-2-bold text-dark-800">
+          More from JS Mastery
+        </div>
+        <Image
+          src="/assets/right-arrow.svg"
+          width={12}
+          height={10}
+          alt="right arrow"
+        />
+      </div>
+
+      <div className="mb-[15px] flex h-[58px] gap-3.5">
+        <Image
+          src="/assets/lightbulb.png"
+          width={58}
+          height={58}
+          alt="post image"
+        />
+        <div className="flex h-[58px] w-full justify-between">
+          <div className="mb-1.5 flex flex-col">
+            <div className="paragraph-4-medium text-dark-800">
+              Showcase Saturday - Unveiling Innovation
+            </div>
+            <div className="text-[10px] text-white-400">by Sujata Gunale</div>
+          </div>
+          <div>
+            <Image
+              src="/assets/right-arrow.svg"
+              width={12}
+              height={10}
+              alt="right arrow"
+            />
+          </div>
+        </div>
+      </div>
+      
+      <div className="mb-[15px] flex h-[58px] gap-3.5">
+        <Image
+          src="/assets/lightbulb.png"
+          width={58}
+          height={58}
+          alt="post image"
+        />
+        <div className="flex h-[58px] w-full justify-between">
+          <div className="mb-1.5 flex flex-col">
+            <div className="paragraph-4-medium text-dark-800">
+              Showcase Saturday - Unveiling Innovation
+            </div>
+            <div className="text-[10px] text-white-400">by Sujata Gunale</div>
+          </div>
+          <div>
+            <Image
+              src="/assets/right-arrow.svg"
+              width={12}
+              height={10}
+              alt="right arrow"
+            />
+          </div>
+        </div>
+      </div>
+     
+    </div>
+  );
 };
 
 export default GroupMorePosts;
