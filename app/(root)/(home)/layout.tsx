52639ef987cd79883b6858c9149f7375a37e6959
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-06-11 18:47:47 +0100

    modify css

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,6 +1,6 @@
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <section className="mx-auto size-full bg-white-200 dark:bg-dark-900 lg:mt-[30px] lg:w-[900px] lg:p-[30px]">
+    <section className="mx-auto size-full min-h-screen bg-white-200 dark:bg-dark-900 lg:mt-[30px] lg:w-[900px] lg:p-[30px]">
       {children}
     </section>
   );
