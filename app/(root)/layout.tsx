f7d381bbca78f6b4775daada9d1a608d0e733d87
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-06-11 18:48:25 +0100

    Add NavigationMobile to the layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/shared/header/Header";
+import {NavigationMobile} from "@/components/shared/NavigationMobile";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -6,6 +7,9 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <Header />
       <div className="size-full bg-white-200 px-[14px] py-[30px] dark:bg-dark-900 sm:px-10 sm:py-5">
         {children}
+      </div>
+      <div className="md:hidden">
+        <NavigationMobile />
       </div>
     </section>
   );
