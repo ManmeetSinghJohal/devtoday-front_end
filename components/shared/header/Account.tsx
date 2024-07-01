627ea303a85126397ebc7b3ba8c7479920242fd7
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-07-01 21:56:56 +0100

    replace localhost:3000 with process.env.DOMAIN_URL

--- components/shared/header/Account.tsx
+++ components/shared/header/Account.tsx
@@ -44,7 +44,7 @@ const Account = () => {
             </div>
           </MenubarTrigger>
           <MenubarContent className="paragraph-3-medium mr-5 mt-3.5 flex w-[174px] flex-col gap-3.5 border-white-border p-5 shadow-none dark:border-dark-700 dark:bg-dark-800 md:rounded-t-none md:border-t-0">
-            <Link href="http://localhost:3000/profile?postType=standard">
+            <Link href={`${process.env.DOMAIN_URL}/profile?postType=standard`}>
               <MenubarItem className="flex cursor-pointer gap-2.5 px-0 dark:text-white-200">
                 <ProfileIcon />
                 <p>Profile</p>
