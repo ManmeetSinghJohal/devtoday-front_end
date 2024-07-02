24a4855d4a424e5a25d703c857fc1f74608daac6
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-07-02 17:49:37 +0100

    modify to use token instead of getSession

--- middleware.ts
+++ middleware.ts
@@ -1,47 +1,92 @@
-import { NextResponse, type NextRequest } from "next/server";
-import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+import { withAuth } from "next-auth/middleware";
 
-export async function middleware(request: NextRequest) {
-  const token = await getToken({
-    req: request,
-    secret: process.env.NEXTAUTH_SECRET,
-  });
+export default withAuth(
+  function middleware(request) {
+    console.log("request.nextauth.token", request.nextauth.token);
+    // console.log("request.nextUrl", request.nextUrl);
+    const { token } = request.nextauth;
+    const { pathname } = request.nextUrl;
 
-  if (
-    !token &&
-    !request.url.includes("signin") &&
-    !request.url.includes("signup")
-  ) {
-    return NextResponse.redirect(new URL("/signin", request.url));
-  }
+    if (!token) {
+      // User is not authenticated
+      if (!pathname.includes("signin") && !pathname.includes("signup")) {
+        return NextResponse.redirect(new URL("/signin", request.url));
+      }
+    } else {
+      // User is authenticated
+      if (
+        token.onboardingCompleted === false &&
+        !pathname.includes("onboarding")
+      ) {
+        return NextResponse.redirect(new URL("/onboarding", request.url));
+      }
+      if (
+        token.onboardingCompleted === true &&
+        pathname.includes("onboarding")
+      ) {
+        return NextResponse.redirect(new URL("/", request.url));
+      }
+      if (pathname.includes("signin") || pathname.includes("signup")) {
+        return NextResponse.redirect(new URL("/", request.url));
+      }
+    }
 
-  if (
-    token &&
-    token.onboardingCompleted === false &&
-    !request.url.includes("onboarding")
-  ) {
-    return NextResponse.redirect(new URL("/onboarding", request.url));
-  }
+    return NextResponse.next();
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+  },
+);
+export const config = { matcher: [] };
 
-  if (
-    token &&
-    token.onboardingCompleted === true &&
-    request.url.includes("onboarding")
-  ) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
+// import { NextResponse, type NextRequest } from "next/server";
+// import { getSession } from "next-auth/react";
 
-  if (
-    token &&
-    token.onboardingCompleted === true &&
-    (request.url.includes("signin") || request.url.includes("signup"))
-  ) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
+// export async function middleware(request: NextRequest) {
+//   // return NextResponse.next();
+//   const requestForNextAuth: any = {
+//     headers: {
+//       cookie: request.headers.get("cookie"),
+//     },
+//   };
+//   const session = await getSession({ req: requestForNextAuth });
 
-  return NextResponse.next();
-}
+//   if (
+//     !session?.user &&
+//     !request.url.includes("signin") &&
+//     !request.url.includes("signup")
+//   ) {
+//     return NextResponse.redirect(new URL("/signin", request.url));
+//   }
 
-export const config = {
-  matcher: "/((?!api|_next/static|_next/image|assets|favicon.ico).*)",
-};
+//   if (
+//     session?.user &&
+//     session?.user.onboardingCompleted === false &&
+//     !request.url.includes("onboarding")
+//   )
+//     return NextResponse.redirect(new URL("/onboarding", request.url));
+
+//   if (
+//     session?.user &&
+//     session?.user.onboardingCompleted === true &&
+//     request.url.includes("onboarding")
+//   )
+//     return NextResponse.redirect(new URL("/", request.url));
+
+//   if (
+//     session?.user &&
+//     session?.user.onboardingCompleted === true &&
+//     (request.url.includes("signin") || request.url.includes("signup"))
+//   ) {
+//     return NextResponse.redirect(new URL("/", request.url));
+//   }
+
+//   return NextResponse.next();
+// }
+
+// export const config = {
+//   matcher: "/((?!api|_next/static|_next/image|assets|favicon.ico).*)",
+// };
