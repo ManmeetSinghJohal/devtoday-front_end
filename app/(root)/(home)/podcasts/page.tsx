cb884f31b384cb9867d176c022d028c225f6b83d
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-07-08 22:36:39 +0100

    This page fetches all podcasts

--- app/(root)/(home)/podcasts/page.tsx
+++ app/(root)/(home)/podcasts/page.tsx
@@ -1,7 +1,39 @@
+import { getServerSession } from "next-auth";
 import React from "react";
 
-const Page = () => {
-  return <div>podcasts</div>;
+import PodcastCard from "@/components/shared/cards/PodcastCard";
+import { authOptions } from "@/lib/auth";
+
+const Page = async () => {
+  const session = await getServerSession(authOptions);
+  const resUser = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/api/user/${session?.user.id}`,
+    {
+      method: "GET",
+      mode: "cors",
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+  const userData = await resUser.json();
+  const posts = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/api/post/PODCAST`,
+    {
+      method: "GET",
+      mode: "cors",
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+  const postsData = await posts.json();
+
+  return (
+    <div className="flex size-full flex-col gap-5 ">
+      <div className="grid grid-cols-1 gap-3.5 lg:grid-cols-2">
+        {postsData.map((post: Post) => {
+          return <PodcastCard user={userData} post={post} key={post.id} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Page;
