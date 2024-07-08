28d7e722b8cd4767c326cf87c0a80d5e8f4a2abb
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-07-08 22:37:02 +0100

    This page fetches all meetups

--- app/(root)/(home)/meetups/page.tsx
+++ app/(root)/(home)/meetups/page.tsx
@@ -1,7 +1,39 @@
+import { getServerSession } from "next-auth";
 import React from "react";
 
-const Page = () => {
-  return <div>meetups</div>;
+import MeetUpCard from "@/components/shared/cards/MeetupCard";
+import { authOptions } from "@/lib/auth";
+
+const Page = async () => {
+  const session = await getServerSession(authOptions);
+  const resUser = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/api/user/${session?.user.id}`,
+    {
+      method: "GET",
+      mode: "cors",
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+  const userData = await resUser.json();
+  const posts = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/api/post/MEETUP`,
+    {
+      method: "GET",
+      mode: "cors",
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+  const postsData = await posts.json();
+
+  return (
+    <div className="flex size-full flex-col gap-5 ">
+      <div className="grid grid-cols-1 gap-3.5">
+        {postsData.map((post: Post) => {
+          return <MeetUpCard user={userData} post={postsData} key={post.id} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Page;
