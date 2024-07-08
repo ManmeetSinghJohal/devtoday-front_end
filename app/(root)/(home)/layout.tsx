4e40f7b2010428a2c815919a2d28e58fbc62ef55
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-07-08 22:38:13 +0100

    Modify layout for community content

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,8 +1,21 @@
+import React from "react";
+
+import LeftSidebar from "@/components/shared/leftsidebar/LeftSidebar";
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <section className="mx-auto size-full min-h-screen bg-white-200 dark:bg-dark-900 lg:mt-[30px] lg:w-[900px] lg:p-[30px]">
-      {children}
-    </section>
+    <div className="flex flex-col gap-5 lg:flex-row">
+      <aside className="relative flex shrink-0 flex-col items-end gap-5 border lg:sticky lg:left-0 lg:top-0 lg:min-w-[210px]">
+        <LeftSidebar />
+      </aside>
+      <div className="flex size-full flex-col gap-5 border">
+        <div>{children}</div>
+      </div>
+
+      <aside className="relative flex flex-col items-end gap-5 border dark:bg-dark-900 lg:sticky lg:right-0 lg:top-0 lg:min-w-[325px]">
+        RightSidebar
+      </aside>
+    </div>
   );
 };
 
