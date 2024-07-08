527bc41978783d2bf7a9e4950edb1b5c3e5592ae
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-07-08 22:37:39 +0100

    This page fetches all standard posts

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,36 @@
+import { getServerSession } from "next-auth";
 import React from "react";
 
-const Home = () => {
-  return <div className="">Home</div>;
+import StandardCard from "@/components/shared/cards/StandardCard";
+import { authOptions } from "@/lib/auth";
+
+const Page = async () => {
+  const session = await getServerSession(authOptions);
+  const resUser = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/api/user/${session?.user.id}`,
+    {
+      method: "GET",
+      mode: "cors",
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+  const userData = await resUser.json();
+  const posts = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/post/STANDARD`, {
+    method: "GET",
+    mode: "cors",
+    headers: { "Content-Type": "application/json" },
+  });
+  const postsData = await posts.json();
+
+  return (
+    <div className="flex size-full flex-col gap-5 ">
+      <div className="grid grid-cols-1 gap-3.5">
+        {postsData.map((post: Post) => {
+          return <StandardCard user={userData} post={post} key={post.id} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
-export default Home;
+export default Page;
