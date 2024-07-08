e9e1ad797bab130412ba13aee4261b35af80c56f
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-07-08 22:35:32 +0100

    Moved file location

--- app/(root)/post/page.tsx
+++ app/(root)/post/page.tsx
@@ -19,10 +19,12 @@ const CreatePost = async () => {
   const groupNames = await resGroups.json();
 
   return (
-    <CreatePostForm
-      groupNames={resGroups.ok ? groupNames : null}
-      authorId={userId}
-    />
+    <section className="mx-auto size-full min-h-screen bg-white-200 dark:bg-dark-900 lg:mt-[30px] lg:w-[900px] lg:p-[30px]">
+      <CreatePostForm
+        groupNames={resGroups.ok ? groupNames : null}
+        authorId={userId}
+      />
+    </section>
   );
 };
 
