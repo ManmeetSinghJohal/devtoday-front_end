487c6af964826d1181ef2afb3c93bbd71df86cd8
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2025-05-24 12:56:43 +0100

    Add profile image uploader with error handling for image updates

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -31,6 +31,38 @@ export const ourFileRouter = {
       console.log("file url", file.url);
       return { uploadedBy: metadata.userId };
     }),
+  profileImageUploader: f({ image: { maxFileSize: "4MB" } })
+    .middleware(async () => {
+      const session = await getServerSession(authOptions);
+      if (!session) throw new UploadThingError("Unauthorized");
+      return { userId: session?.user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      try {
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_SERVER_URL}/api/profile/profile-image/${metadata.userId}`,
+          {
+            method: "PATCH",
+            mode: "cors",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              image: file.url,
+            }),
+          },
+        );
+        if (!res.ok) {
+          const text = await res.text(); // fallback to plain text in case it's HTML
+          throw new Error(
+            `Failed to update image. Status: ${res.status}, Body: ${text}`,
+          );
+        }
+        // const data = await res.json();
+      } catch (error) {
+        console.log(error);
+      }
+    }),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
